65d04bbf635461fde306db03e472bd6da9a5a1ab
Chain Qian <chainz@foxmail.com>
2017-08-08 17:05:22 +0800

    update

--- Chain-Utils/src/com/chain/exception/ChainUtilsRuntimeException.java
+++ Chain-Utils/src/com/chain/exception/ChainUtilsRuntimeException.java
@@ -1,6 +1,11 @@
 package com.chain.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ChainUtilsRuntimeException extends RuntimeException {
+
+	private static final Logger logger = LoggerFactory.getLogger(ChainUtilsRuntimeException.class);
 
 	/**
 	 * 
@@ -14,14 +19,17 @@ public class ChainUtilsRuntimeException extends RuntimeException {
 	public ChainUtilsRuntimeException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
+		logger.warn(message, cause);
 	}
 
 	public ChainUtilsRuntimeException(String message, Throwable cause) {
 		super(message, cause);
+		logger.warn(message, cause);
 	}
 
 	public ChainUtilsRuntimeException(String message) {
 		super(message);
+		logger.warn(message);
 	}
 
 	public ChainUtilsRuntimeException(Throwable cause) {
